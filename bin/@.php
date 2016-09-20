da0d960c06e1fc8461a7764aa105ba5e337f79a4
Yakov Lachin <yashalachin@gmail.com>
2016-09-21 02:05:54 +0300

    правки @.php

--- bin/@.php
+++ bin/@.php
@@ -1,34 +1,7 @@
 #!/usr/bin/php
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
-
-
-$consumers = array(
-    "mrBadger",
-    "v",
-    "dghost",
-    "domio",
-    "irvis",
-    "ilia",
-    "max",
-    "alexander",
-    "cnam",
-    "moshhh",
-    "slack/me",
-    "slack/barabule4ka",
-    "telegram"
-);
-
-
-function consumers() {
-    global $consumers;
-
-    return $consumers;
-};
-
-
 consumeInput($argv);
-
 function consumeInput($input) {
     $bootstrap      = \Messenger\Bootstrap::getInstance();
     /**
